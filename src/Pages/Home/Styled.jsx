5ecebcbcdee5f08bc40134772b5f5ed06d12ed5a
Eliott Raheriarisoa <eliott.raheriarisoa@gmail.com>
2022-07-03 00:16:47 +0200

    fix margin

--- src/Pages/Home/Styled.jsx
+++ src/Pages/Home/Styled.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components"
 export const ContainerReservation = styled.div`
   display: flex;
   overflow: scroll;
-  padding: 0 1.5rem;
+  padding-left: 1.5rem;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
 `
